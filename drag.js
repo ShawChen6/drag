a74f34f9cbed2b2c419f418150c80afc64ce034f
ShawChen6 <932769985@qq.com>
2019-01-12 14:17:06 +0800

    change drag.js

--- drag.js
+++ drag.js
@@ -2,5 +2,8 @@ function drag(id){
 	var obj = document.getElementById(id);
 	var a = 0;
 	var b = 0;
-	obj.onmousedown = function()
+	obj.onmousedown = function(ev){
+		a = ev.pageX - obj.offsetLeft;
+		b = ev.pageY - obj.offsetTop;
+	}
 }
